6023a4b88d593163b4f4c5951b8b5988f1274940
LucasSarri <lucassarri@hotmail.com>
2025-02-06 07:23:55 -0400

    alteracoes dia 06-02-2025

--- src/controllers/GamificacaoController.ts
+++ src/controllers/GamificacaoController.ts
@@ -2,47 +2,47 @@ import { Request, Response } from "express";
 import { cenarioDesejadoRepository } from "../repositories/cenarioDesejadoRepository";
 import { planejamentoRepository } from "../repositories/planejamentoRepository";
 import { conceitosScrumRepository } from "../repositories/conceitosScrumRepository";
+import { gamificacaoRepository } from "../repositories/gamificacaoRepository";
 
 
 export class GamificacaoController {
     async RenderGamificacao (req: Request, res: Response) {
         const parametro = req.params.parametro;
-        console.log(parametro);
-/*
-        const conceitos_scrum = await conceitosScrumRepository
-        .createQueryBuilder("scrum")
-        .innerJoinAndSelect("scrum.idAgScrum", "ag_scrum")
+
+        const gamificacao = await gamificacaoRepository
+        .createQueryBuilder("gamificacao")
+        .innerJoinAndSelect("gamificacao.ag_gamificacao", "ag_gamificacao")
         .getMany();
 
-        return res.status(200).render(`formConceitosScrum`, {conceitos_scrum, planejamento: parametro});*/
+        return res.status(200).render(`formElementosGamificacao`, {gamificacao, planejamento: parametro});
     }
 
     async addGamificacaoPlanejamento (req: Request, res: Response) {
         const corpo = req.body;
-
+    
         const planejamento = await planejamentoRepository.findOne({
             where: {id: corpo.idPlanejamento},
-            relations: ['scrums']
+            relations: ['elementos_gamificacao']
         });
 
         if (!planejamento) {
             throw new Error(`Planejamento com ID ${corpo.idPlanejamento} não encontrado.`);
         }
 
-        const scrums = await conceitosScrumRepository.findByIds(corpo.ativo);
+        const elementos_gamificacao = await gamificacaoRepository.findByIds(corpo.ativo);
 
-        if (scrums.length === 0) {
+        if (elementos_gamificacao.length === 0) {
             throw new Error('Nenhum dos cenários fornecidos foi encontrado.');
         }
 
-        scrums.forEach((scrum) => {
-            if (!planejamento.scrums.some((c) => c.id === scrum.id)) {
-                planejamento.scrums.push(scrum);
+        elementos_gamificacao.forEach((elemento_gamificacao) => {
+            if (!planejamento.elementos_gamificacao.some((c) => c.id === elemento_gamificacao.id)) {
+                planejamento.elementos_gamificacao.push(elemento_gamificacao);
             }
         });
 
         await planejamentoRepository.save(planejamento);
     
-        return res.status(200).redirect(`/perfil_scrum/${corpo.idPlanejamento}`);
+        //return res.status(200).redirect(`/perfil_scrum/${corpo.idPlanejamento}`);
     }
 }
